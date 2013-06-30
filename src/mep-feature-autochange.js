ff867e74a7045d0f6b6439fec4ae4c1ddde23fbd
Tristan Rivoallan <tristan@rivoallan.net>
2013-06-30 08:43:45 +0200

    [debt] removed a remaining "console.log" call

--- src/mep-feature-autochange.js
+++ src/mep-feature-autochange.js
@@ -14,7 +14,6 @@
   $.extend(MediaElementPlayer.prototype, {
     buildautochange: function(player, controls, layers, media) {
       media.addEventListener('ended', function() {
-        console.log($(player.options.autochangeSelectorNextLink));
         if ($(player.options.autochangeSelectorNextLink).length) {
           window.location = $(player.options.autochangeSelectorNextLink).attr('href') + '?' + player.options.autochangeQueryString;
         }
